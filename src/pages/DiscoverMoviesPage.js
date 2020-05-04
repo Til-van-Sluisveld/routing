e1e794422ae55e701e0f92984c1798499e758810
Til-van-sluisveld <Tilvansluisveld@gmail.com>
2020-05-04 15:13:18 +0200

    Added (URL) location as state in discover page

--- src/pages/DiscoverMoviesPage.js
+++ src/pages/DiscoverMoviesPage.js
@@ -1,33 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useHistory, useParams } from "react-router-dom";
 
 export default function DiscoverMoviesPage() {
   const [searchText, set_searchText] = useState("");
 
   const [searchState, set_searchState] = useState({ status: "idle" });
 
-  const search = async () => {
-    console.log("Start searching for:", searchText);
+  const history = useHistory();
+  const check_params = useParams();
+
+  const navigateToSearch = () => {
+    const routeParam = encodeURIComponent(searchText);
+    console.log("routeParam is:", routeParam);
+    history.push(`/discover/${routeParam}`);
+  };
+  console.log("check_params is:", check_params);
+
+  useEffect(() => {
+    console.log("useEffect triggered");
+    //first check to see if there is a search query in the url, if not return basic discover movies page
+    if (check_params.searchtext) {
+      console.log("input is:", check_params.searchtext);
+      search(check_params.searchtext);
+    } else {
+      console.log("no input found");
+      set_searchState({ status: "idle" });
+    }
+  }, [check_params]);
+
+  const search = async (input) => {
+    console.log("Start searching for:", input);
     set_searchState({ status: "searching" });
-    console.log("searchState is:", searchState);
     // Best practice: encode the string so that special characters
     //  like '&' and '?' don't accidentally mess up the URL
-    const queryParam = encodeURIComponent(searchText);
-
-    // Option A: use the browser-native fetch function
-    // const data = await fetch(
-    //   `https://omdbapi.com/?apikey=120927c9&s=${queryParam}`
-    // ).then((r) => r.json());
+    const queryParam = encodeURIComponent(input);
 
     // Option B: use the `axios` library like we did on Tuesday
     const data = await axios.get(
       `https://omdbapi.com/?apikey=120927c9&s=${queryParam}`
     );
-
+    // data from axios is stored in data but needs to be transformed to an array (list of movies)
     set_searchState({ status: "done", data: data.data.Search });
-
-    console.log("Success!", data.data.Search);
   };
 
   console.log("searchState is:", searchState);
@@ -40,7 +54,7 @@ export default function DiscoverMoviesPage() {
     display = searchState.data.map((movie) => {
       return (
         <div className="movie">
-          <Link to={`discover/${movie.imdbID}`}>
+          <Link to={`/movie/${movie.imdbID}`}>
             <h4 key={movie.imdbID}>{movie.Title}</h4>
           </Link>
           <p> Year of release: {movie.Year}</p>
@@ -58,7 +72,7 @@ export default function DiscoverMoviesPage() {
           value={searchText}
           onChange={(e) => set_searchText(e.target.value)}
         />
-        <button onClick={search}>Search</button>
+        <button onClick={navigateToSearch}>Search</button>
       </p>
       {display}
     </div>
